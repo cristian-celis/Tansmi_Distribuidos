af9d9327c93867e157f8ff12a744c2d959469fad
cristian-celis <jesuscelis468@gmail.com>
2024-09-01 07:49:18 -0500

    First commit: Add server.js

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+let buses = {};
+
+app.listen(3000, () => {
+    console.log('Servidor corriendo en el puerto 3000');
+});
+
+app.post('/buses', (req, res) => {
+    const { plate } = req.body;
+
+    if (!plate) {
+        return res.status(400).send('La placa es requerida.');
+    }
+
+    const normalizedPlate = plate.toUpperCase(); // Convertir la placa a mayusculas
+    const arrivalTime = new Date().toISOString(); // Obtener tiempo llegada
+
+    if (buses[normalizedPlate]) {
+        buses[normalizedPlate].arrivalTime = arrivalTime;
+        buses[normalizedPlate].edits += 1;
+    } else {
+        buses[normalizedPlate] = { arrivalTime, edits: 0 };
+    }
+
+    res.send(`Bus con placa ${normalizedPlate} registrado/actualizado a las ${arrivalTime}.`);
+});
+
+app.delete('/buses/:plate', (req, res) => {
+    const plate = req.params.plate.toUpperCase();
+
+    if (buses[plate]) {
+        delete buses[plate];
+        res.send(`Bus con placa ${plate} eliminado.`);
+    } else {
+        res.status(404).send('Bus no encontrado.');
+    }
+});
+
+app.get('/buses/:plate', (req, res) => {
+    const plate = req.params.plate.toUpperCase()
+
+    if (buses[plate]) {
+        res.send(`El bus con placa ${plate} ha sido editado ${buses[plate].edits} veces.`);
+    } else {
+        res.status(404).send('Bus no encontrado.');
+    }
+});
